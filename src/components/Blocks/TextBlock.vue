90ea9a3117b7f3efdbda8fd5fe099c1eda1fd6f5
Alfarish Fizikri <alfarish@evermos.com>
2025-01-19 09:40:21 +0700

    feat(textBlock): adjust default text and implement color prop

--- src/components/Blocks/TextBlock.vue
+++ src/components/Blocks/TextBlock.vue
@@ -10,7 +10,7 @@ interface Props {
 
 const props = defineProps<Props>()
 const customVPadding = computed(() => `pt-${props?.topPadding ?? 10} pb-${props?.bottomPadding ?? 10}`)
-const data = ref('')
+const data = ref('Write engaging and informative content that will help your readers understand your message.')
 
 function updateText(event) {
   data.value = event.target.innerHTML
@@ -21,12 +21,12 @@ function updateText(event) {
 
 <template>
   <div :class="`p-4 border ${customVPadding} rounded bg-[${props?.backgroundColor ?? '#808080'}]`">
-    <div contenteditable="true" class="border p-2" @input="updateText" v-html="data" />
+    <div :class="`text-${props?.color ?? '#000000'}`" contenteditable="true" class="border p-2" @input="updateText" v-html="data" />
   </div>
 </template>
 
 <style scoped>
 [contenteditable]:focus {
-  outline: 0px solid transparent;
+  outline: 0 solid transparent;
 }
 </style>
