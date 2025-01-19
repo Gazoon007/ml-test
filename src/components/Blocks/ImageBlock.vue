f4f25a633c8344c3c3255fa045ded75fbef04d5f
Alfarish Fizikri <alfarish@evermos.com>
2025-01-19 17:08:09 +0700

    feat(imageBlock): add modal for select predefined images

--- src/components/Blocks/ImageBlock.vue
+++ src/components/Blocks/ImageBlock.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import ImageSelectModal from '@/components/ImageSelectModal.vue'
 import { useBlockStore } from '@/store/blocks'
 import { computed, ref } from 'vue'
 
@@ -11,34 +12,25 @@ interface Props {
 }
 
 const props = defineProps<Props>()
-const fileInput = ref<HTMLInputElement | null>(null)
 const images = ref<(string | null)[]>(Array.from<string | null>({ length: props?.imageCount ?? 3 }).fill(null))
 const imageSlots = computed(() => images.value)
 const customPX = computed(() => props.galleryLayout === 'default' ? 'px-50px gap-4' : props.galleryLayout === 'spaceless' ? 'px-50px' : 'px-0')
 const customVPadding = computed(() => `pt-${props?.topPadding ?? 0} pb-${props?.bottomPadding ?? 0}`)
+const isModalOpen = ref(false)
+const currentImageIndex = ref(null)
 
-function handleUpload(index: number): void {
-  if (fileInput.value) {
-    fileInput.value.dataset.index = index.toString()
-    fileInput.value.click()
-  }
+function openModal(index: number) {
+  currentImageIndex.value = index
+  isModalOpen.value = true
 }
 
-function handleFileChange(event: Event): void {
-  const target = event.target as HTMLInputElement
-  const index = Number.parseInt(target.dataset.index || '0', 10)
-  const file = target.files?.[0]
+function closeModal() {
+  currentImageIndex.value = null
+  isModalOpen.value = false
+}
 
-  if (file) {
-    const reader = new FileReader()
-    reader.onload = (e) => {
-      images.value[index] = e.target?.result as string
-    }
-    reader.readAsDataURL(file)
-  }
-
-  if (target)
-    target.value = ''
+function handleImageSelected(image: string) {
+  images.value[currentImageIndex.value] = image
 
   useBlockStore().setBlocks({
     id: Date.now().toString(),
@@ -59,7 +51,7 @@ function handleFileChange(event: Event): void {
       v-for="(image, index) in imageSlots"
       :key="index"
       class="h-32 border border-dashed border-gray-400 rounded-md flex items-center justify-center cursor-pointer"
-      @click="handleUpload(index)"
+      @click="openModal(index)"
     >
       <template v-if="image">
         <img :src="image" alt="Uploaded image" class="h-full object-cover rounded-md min-w-126px">
@@ -68,12 +60,10 @@ function handleFileChange(event: Event): void {
         <img src="/src/assets/placeholder-image-block.jpg" alt="=placeholder" class="text-gray-500 text-sm w-32">
       </template>
     </div>
-    <input
-      ref="fileInput"
-      type="file"
-      class="hidden"
-      accept="image/*"
-      @change="handleFileChange"
-    >
+    <ImageSelectModal
+      :is-open="isModalOpen"
+      @close="closeModal"
+      @image-selected="handleImageSelected"
+    />
   </div>
 </template>
---
+++ src/components/ImageSelectModal.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { defineEmits, defineProps } from 'vue'
+
+defineProps({
+  isOpen: {
+    type: Boolean,
+    required: true,
+  },
+})
+const emit = defineEmits(['close', 'imageSelected'])
+
+function closeModal() {
+  emit('close')
+}
+
+function selectImage(image) {
+  emit('imageSelected', `/mailerlite-family-photos/${image}`)
+  closeModal()
+}
+</script>
+
+<template>
+  <div v-if="isOpen" class="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-75">
+    <div class="bg-white rounded-lg p-4 shadow-lg w-auto">
+      <h2 class="text-lg font-bold mb-4">
+        Select an Image
+      </h2>
+      <div class="grid grid-cols-3 gap-4">
+        <img
+          v-for="image in ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg']"
+          :key="image"
+          :src="`/mailerlite-family-photos/${image}`"
+          :alt="image"
+          class="cursor-pointer hover:opacity-75 w-64"
+          @click="selectImage(image)"
+        >
+      </div>
+      <button class="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700" @click="closeModal">
+        Close
+      </button>
+    </div>
+  </div>
+</template>
