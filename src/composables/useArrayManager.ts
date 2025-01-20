6ed79ef3b28b8762bca5d20849b64c72b4a370ac
Alfarish Fizikri <alfarish@evermos.com>
2025-01-20 11:46:22 +0700

    feat: init useArrayManager

---
+++ src/composables/useArrayManager.ts
@@ -0,0 +1,64 @@
+import { ref } from 'vue'
+
+export function useArrayManager(initialArray = []) {
+  const array = ref(initialArray)
+
+  const moveUp = (index) => {
+    if (index > 0) {
+      const newArr = [...array.value];
+      [newArr[index], newArr[index - 1]] = [newArr[index - 1], newArr[index]]
+      array.value = newArr
+    }
+  }
+
+  const moveDown = (index) => {
+    if (index < array.value.length - 1) {
+      const newArr = [...array.value];
+      [newArr[index], newArr[index + 1]] = [newArr[index + 1], newArr[index]]
+      array.value = newArr
+    }
+  }
+
+  const duplicate = (index) => {
+    if (index >= 0 && index < array.value.length) {
+      const newArr = [...array.value]
+      const newItem = ref({ ...newArr[index] })
+      newArr.splice(index + 1, 0, newItem.value)
+      array.value = newArr
+    }
+  }
+
+  const remove = (index) => {
+    if (index >= 0 && index < array.value.length) {
+      const newArr = [...array.value]
+      newArr.splice(index, 1)
+      array.value = newArr
+    }
+  }
+
+  const insert = (newItem, index) => {
+    const newArr = [...array.value]
+    if (index >= 0 && index <= newArr.length) {
+      newArr.splice(index, 0, { ...newItem })
+      array.value = newArr
+    }
+  }
+
+  const replace = (newItem, index) => {
+    if (index >= 0 && index < array.value.length) {
+      const newArr = [...array.value]
+      newArr[index] = { ...newItem }
+      array.value = newArr
+    }
+  }
+
+  return {
+    mutatedArray: array,
+    moveUp,
+    moveDown,
+    duplicate,
+    remove,
+    insert,
+    replace,
+  }
+}
